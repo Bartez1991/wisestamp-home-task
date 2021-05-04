eb7b96203a9535351c024631fdf83b44d94acbed
ihor_z <zapisotskiy.igor@gmail.com>
2021-05-04 20:06:17 +0300

    Countdown Timer & Change Title Color

--- twentytwentyone-child/lib/ScriptsAndStyles.php
+++ twentytwentyone-child/lib/ScriptsAndStyles.php
@@ -7,12 +7,13 @@ Class ScriptsAndStyles{
         add_action( 'wp_enqueue_scripts', 'ScriptsAndStyles::addThemeStyles');
     }
 
-    static function addThemeScripts(){       
+    static function addThemeScripts(){        
         wp_enqueue_script( 'momentjs', 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js', array(), null, true);
+        wp_enqueue_script( 'child-script', get_stylesheet_directory_uri() . '/assets/public/theme.min.js', array(), '', true);
     }
 
     static function addThemeStyles() {
-        wp_enqueue_style('child-css', get_stylesheet_directory_uri() . '/assets/public/theme.min.css', array('twenty-twenty-one-style'), '', 'screen');
+        wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/assets/public/theme.min.css', array('twenty-twenty-one-style'), '', 'screen');
     }
 
 }
--- twentytwentyone-child/partials/block-hero.php
+++ twentytwentyone-child/partials/block-hero.php
@@ -14,7 +14,7 @@ $colors  = array(
   'green'   => '#93f1aa',
   'red'     => '#cb1a1a',
   'yellow'  => '#eade5d',
-  'blue'    => '5d95ea'
+  'blue'    => '#5d95ea'
 );
 
 ?>
@@ -22,19 +22,27 @@ $colors  = array(
 <section class="<?php echo esc_attr($classes); ?>">
   <div class="section-inner">
     <div class="wrapper">
-      <?php if ($heading) : ?>
-        <h2 class="h2 title-hero"><?php echo $heading; ?></h2>
-      <?php endif; ?>
+      <div class="hero-content">
 
-      <?php if ($colors) : ?>
-        <div class="btn-group">
-          <?php foreach ($colors as $color => $value) : ?>
-            <div class="btn-item">
-              <button class="btn btn-<?php echo $color; ?>" data-color="<?php echo $value; ?>"><?php echo ucfirst($color); ?></button>
-            </div>
-          <?php endforeach; ?>
+
+        <?php if ($heading) : ?>
+          <h2 class="h2 title-hero"><?php echo $heading; ?></h2>
+        <?php endif; ?>
+
+        <div class="countdown">
+          <p class="countdown-label"><?php _e('Countdown', 'twentytwentyone'); ?></p>
         </div>
-      <?php endif; ?>
+
+        <?php if ($colors) : ?>
+          <div class="btn-group button-colors">
+            <?php foreach ($colors as $color => $value) : ?>
+              <div class="btn-item">
+                <button class="button-color btn btn-<?php echo $color; ?>" data-color="<?php echo $value; ?>"><?php echo ucfirst($color); ?></button>
+              </div>
+            <?php endforeach; ?>
+          </div>
+        <?php endif; ?>
+      </div>
     </div>
   </div>
 
